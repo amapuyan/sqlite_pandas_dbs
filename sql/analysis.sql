840472035e86b4ceaf973c16372be496639d4750
Anthony Mapuyan <amapuyan@MacBook-Pro-5.local>
2025-10-09 02:27:32 -0400

    final checks

--- sql/analysis.sql
+++ sql/analysis.sql
@@ -1,18 +1,19 @@
+-- A
 SELECT COUNT(*) AS n_patients FROM patients;
 
-
+-- B
 SELECT primary_icd10, COUNT(*) AS n
 FROM patients
 GROUP BY primary_icd10
 ORDER BY n DESC;
 
-
+-- C
 SELECT patient_id, last_cpt, last_visit_dt
 FROM patients
 WHERE last_cpt LIKE '992%' AND last_visit_dt >= '2025-01-01'
 ORDER BY last_visit_dt DESC;
 
-
+-- D
 SELECT
   patient_id,
   birth_date,
@@ -22,7 +23,7 @@ FROM patients
 ORDER BY age_years DESC
 LIMIT 5;
 
-
+-- E
 SELECT *
 FROM patients
 WHERE primary_icd10 = '' OR last_cpt = '';
